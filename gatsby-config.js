0ba90c149c6d6f6c75164be15a53bc7f293add83
PeHaa <info@pehaa.com>
2021-10-04 14:07:43 +0200

    no shadowing

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,15 +4,6 @@ module.exports = {
       resolve: "@pehaa/gatsby-theme-wp-child",
       options: {
         wordPressUrl: "https://pehaa.xyz/gatsby-demo",
-        fonts: {
-          google: [
-            {
-              family: "Rubik",
-              variants: ["400", "400i", "700"],
-              fontDisplay: "swap",
-            },
-          ],
-        },
       },
     },
   ],
